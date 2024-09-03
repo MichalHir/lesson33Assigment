e68a87eb0475865547f56a4c15b3f8fdcc65b51b
MichalHir <michalaizi@gmail.com>
2024-09-03 21:14:13 +0300

    opp flask alchemy

--- oop_1/book/models.py
+++ oop_1/book/models.py
@@ -1,24 +1,33 @@
-# loan_book(user), return_book()
+from datetime import datetime, timedelta
 class Book:
     def __init__(self, name, author):
         self.name = name
         self.author = author
         self.user = None
+        self.loan_date= None
         print("creating a new book")
 
     def __str__(self):
         return f"name:{self.name}"
-    
-    def loan_book(self,user):
-        if self.user is None:
-            self.user = user
-            user.loan_book()
-        else:
-            print("Book is already loaned out.")
 
-    def return_book(self):
+    def loan_book(self, user):
+        # if self.user is None:
+        #     self.user = user
+        #     user.loan_book()
+        # else:
+        #     print("Book is already loaned out.")
+        self.user = user 
+        self.loan_date= datetime.today()
+        print("RETURN BY ",self.loan_date+timedelta(days=10))
+    def return_book(self,date_of_return):
+        if not date_of_return:
+            date_of_return = datetime.today()
+        if not date_of_return>self.loan_date+timedelta(days=10):
+            print("50 shekels fine")
         print(f"{self.name} was returned by {self.user}")
         self.user = None
+        self.loan_date=None
+
     # def return_book(self, user):
     #     if self.user == user:
     #         self.user.check_late()
@@ -26,4 +35,3 @@ class Book:
     #         user.date_of_loan = None
     #     else:
     #         print("This book was not loaned to you.")
-
--- oop_1/user/models.py
+++ oop_1/user/models.py
@@ -1,29 +1,29 @@
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 
 
 class User:
     def __init__(self, user_name, number):
         self.name = user_name
         self.number = number
-        self.date_of_loan=None
+        # self.date_of_loan=None
         print("creating a new user")
     def __str__(self):
         return f"name:{self.name}"
-    def loan_book(self):
-        if self.date_of_loan is None:
-            self.date_of_loan = datetime.now()
-            return_date = self.date_of_loan + timedelta(days=10)
-            print(f"Return date: {return_date.strftime('%Y-%m-%d %H:%M:%S')}")
-        else:
-            print("You already have a book loaned out.")
+    # def loan_book(self):
+    #     if self.date_of_loan is None:
+    #         self.date_of_loan = datetime.now()
+    #         return_date = self.date_of_loan + timedelta(days=10)
+    #         print(f"Return date: {return_date.strftime('%Y-%m-%d %H:%M:%S')}")
+    #     else:
+    #         print("You already have a book loaned out.")
 
-    def check_late(self):
-        if self.date_of_loan:
-            current_date = datetime.now()
-            due_date = self.date_of_loan + timedelta(days=10)
-            if current_date > due_date:
-                print("YOU ARE LATE. The fine is 50 shekel.")
-            else:
-                print("Book is returned on time.")
-        else:
-            print("No book is currently loaned.")
+    # def check_late(self):
+    #     if self.date_of_loan:
+    #         current_date = datetime.now()
+    #         due_date = self.date_of_loan + timedelta(days=10)
+    #         if current_date > due_date:
+    #             print("YOU ARE LATE. The fine is 50 shekel.")
+    #         else:
+    #             print("Book is returned on time.")
+    #     else:
+    #         print("No book is currently loaned.")
